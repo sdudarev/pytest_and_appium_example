d42ae973c36709420f1cb174cee1a7618540afe4
mi <sdudarev>
2020-11-03 00:38:00 +0300

    В дибеге 47 строку получается запускать, а если без дибага запускать, то тест падает(

--- conftest.py
+++ conftest.py
@@ -12,9 +12,24 @@ desired_caps = dict(
     app=os.path.abspath(os.path.join(__file__, '..', 'ru.europlan.lkk.apk'))
 )
 
+desired_win = dict(
+    platformName='Android',
+    platformVersion='8',
+    deviceName='Android Emulator',
+    appWaitActivity='ru.internetinvestments.*',
+    app=os.path.abspath(os.path.join(__file__, '..', 'Winner.Stage_base.apk'))
+)
+
 
 @pytest.fixture()
 def android_driver():
     driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+    yield driver
+    driver.quit()
+
+
+@pytest.fixture()
+def android_winner():
+    driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_win)
     yield driver
     driver.quit()
--- tests/test_example.py
+++ tests/test_example.py
@@ -35,3 +35,38 @@ class TestExamples:
         assert err_texts[0].text == 'Введите имя пользователя'
         assert err_texts[1].text == 'Введите пароль'
 
+    def test_example_4(self, android_driver):
+        android_driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout'
+                                             '/android.widget.FrameLayout/android.widget.LinearLayout/android'
+                                             '.widget.FrameLayout/android.view.ViewGroup/android.widget'
+                                             '.LinearLayout/android.widget.Button[2]').click()
+
+        android_driver.implicitly_wait(3)
+
+    def test_winner_1(self, android_winner):
+        android_winner.find_elements_by_class_name('android.widget.EditText')[0].send_keys('vvd1')
+        password = android_winner.find_elements_by_class_name('android.widget.EditText')[1]
+        password.click()
+        password.send_keys('qwerty123')
+        android_winner.back()
+        android_winner.implicitly_wait(5)
+        android_winner.find_element_by_class_name('android.widget.Button').click()
+        test = android_winner.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget'
+                                                    '.LinearLayout/android.widget.FrameLayout/android.widget'
+                                                    '.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup'
+                                                    '/android.widget.TextView[2]').text
+        test_2 = android_winner.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget'
+                                                      '.LinearLayout/android.widget.FrameLayout/android.widget'
+                                                      '.LinearLayout/android.widget.FrameLayout/android.view'
+                                                      '.ViewGroup/android.widget.TextView[3]').text
+        assert test == 'Задайте код доступа'
+        assert test_2 == 'Повторите код доступа'
+
+        android_winner.find_element_by_class_name('android.widget.Button')[7].click()
+
+        test_3 = android_winner.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget'
+                                                      '.LinearLayout/android.widget.FrameLayout/android.widget'
+                                                      '.LinearLayout/android.widget.FrameLayout/android.widget'
+                                                      '.RelativeLayout/android.widget.ScrollView/android.widget'
+                                                      '.LinearLayout/android.widget.TextView[2]').text
+        assert test_3 == 'Личный кабинет'
